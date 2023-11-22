2705612f183c24f3c2152167e80dd4af41cab55d
Chuck Jungmann <chuck@cpjj.net>
2023-11-22 16:26:53 -0600

    bulk changes/furthering development with experimentation
    Implement scrolling using terminal scrolling commands.  Solved
    ignore keypress problem.

--- pager.c
+++ pager.c
@@ -1,7 +1,65 @@
 #include <stdbool.h>
+#include <string.h>
+#include <unistd.h>
+#include <curses.h>
+#include <term.h>
+#include <assert.h>
 
 #include "pager.h"
 #include "screen.h"
+#include "get_keystroke.h"
+
+PKMAP sel_keymap[] = {
+   { PAGER_EXIT,      "q" },         // 'q' key to quit
+   { PAGER_SELECT,    "\x0D" },      // NEWLINE (ENTER key) to select (\x0D CR, \x0A for NL)
+   { FOCUS_DOWN_ONE,  "kcud1" },     // down key
+   { FOCUS_UP_ONE,    "kcuu1" },     // up key
+   { FOCUS_HOME,      "khome" },     // home key
+   { FOCUS_END,       "kend" },      // end key
+   { FOCUS_DOWN_PAGE, "knp" },       // next-page key (pgdn)
+   { FOCUS_UP_PAGE,   "kpp" },       // previous-page key (pgup)
+   { -1 }
+};
+
+PACTION const sel_actionmap[] = {
+   pager_quit,
+   pager_activate,
+   pager_focus_down_one,
+   pager_focus_up_one,
+   pager_focus_down_page,
+   pager_focus_up_page,
+   pager_focus_end,
+   pager_focus_home,
+   pager_scroll_down_one,
+   pager_scroll_up_one,
+   pager_scroll_down_page,
+   pager_scroll_up_page,
+   pager_scroll_end,
+   pager_scroll_home,
+   NULL
+};
+
+
+
+void update_keymap_values(PKMAP *kmap, int el_size)
+{
+   char *ptr = (char*)kmap;
+   PKMAP *pkm;
+   while ((pkm=(PKMAP*)ptr)->action_index >= 0)
+   {
+      const char *name = pkm->capname;
+      if (strlen(name) > 1 && name[0] == 'k')
+      {
+         char *val = tigetstr(pkm->capname);
+         if (val)
+            pkm->value = val;
+      }
+      else
+         pkm->value = name;
+
+      ptr += el_size;
+   }
+}
 
 void update_display_params(DPARMS *params)
 {
@@ -12,12 +70,55 @@ void update_display_params(DPARMS *params)
    params->line_count = rows - params->reserve_top - params->reserve_bottom;
    params->chars_left = params->reserve_left;
    params->chars_count = cols - params->reserve_left - params->reserve_right;
+
+   set_scroll_limits(params->line_top, rows - params->reserve_bottom - 1);
+}
+
+void print_indexed_row(const DPARMS *parms, int row_index, bool has_focus)
+{
+   // static int prow, pcol;
+   // get_screen_size(&prow, &pcol);
+   int line = parms->line_top + (row_index - parms->index_row_top);
+   set_cursor_position(line, parms->chars_left);
+   (parms->printer)(row_index, has_focus, parms->chars_count, parms->data_source);
+}
+
+PACTION page_get_action(const DPARMS *parms, const char *keystroke)
+{
+   int ndx = -1;
+   const PKMAP *ptr = parms->keymap;
+   while (ptr->action_index >= 0)
+   {
+      if (0==strcmp(keystroke, ptr->value))
+      {
+         ndx = ptr->action_index;
+         break;
+      }
+      ++ptr;
+   }
+   return parms->actions[ndx];
+}
+
+void scroll_line_up(const DPARMS *parms)
+{
+   assert(tis_values_set());
+   int col = parms->chars_left + parms->chars_count ;
+   int row = parms->line_top + parms->line_count - 1;
+   set_cursor_position(row, col);
+   screen_write_str(tis_scroll_forward, STDOUT_FILENO);
+}
+
+void scroll_line_down(const DPARMS *parms)
+{
+   assert(tis_values_set());
+   set_cursor_position(parms->line_top, 1);
+   screen_write_str(tis_scroll_reverse, STDOUT_FILENO);
 }
 
 void print_page(DPARMS *params)
 {
    update_display_params(params);
-   
+
    int left = params->chars_left;
    int line = params->line_top;
    int chars_count = params->chars_count;
@@ -29,9 +130,43 @@ void print_page(DPARMS *params)
 
    for ( ; row < end_row; ++row, ++line)
    {
-      bool selected = row == params->index_row_selected;
+      bool has_focus = row == params->index_row_focus;
       set_cursor_position(line, left);
-      (params->printer)(row, selected, chars_count, params->data_source);
+      (params->printer)(row, has_focus, chars_count, params->data_source);
    }
+}
+
+
+void start_pager(DPARMS *params)
+{
+   reset_screen();
+   print_page(params);
+
+   char kbuff[20];
+   ARV action_return = ARV_CONTINUE;
+
+   while (action_return != ARV_EXIT)
+   {
+      char *keys = get_keystroke(kbuff, sizeof(kbuff));
+      if (keys)
+      {
+         PACTION action = page_get_action(params, keys);
+         if (action)
+            action_return = (*action)(params);
+         else
+            action_return = ARV_CONTINUE;
+      }
+   }
+}
+
+bool row_index_is_visible(const DPARMS *parms, int row_index)
+{
+   int bottom_row= parms->index_row_top + parms->line_count;
+   return row_index >= parms->index_row_top && row_index < bottom_row;
+}
+
+int line_index_from_row_index(const DPARMS *parms, int row_index)
+{
+   return row_index - parms->index_row_top;
 }
 
--- pager.h
+++ pager.h
@@ -1,9 +1,47 @@
 #ifndef PAGER_H
 #define PAGER_H
 
+#include <stdbool.h>
+
+enum drive_values {
+   PAGER_EXIT = 0,
+   PAGER_SELECT,
+   FOCUS_DOWN_ONE,
+   FOCUS_UP_ONE,
+   FOCUS_DOWN_PAGE,
+   FOCUS_UP_PAGE,
+   FOCUS_END,
+   FOCUS_HOME,
+   SCROLL_DOWN_ONE,
+   SCROLL_UP_ONE,
+   SCROLL_DOWN_PAGE,
+   SCROLL_UP_PAGE,
+   SCROLL_END,
+   SCROLL_HOME
+};
+
+
+typedef struct pager_keymap PKMAP;
 typedef struct display_params DPARMS;
 
-typedef int (*pwb_print_line)(int row_index, int indicated, int length, void *dsource);
+typedef int (*pwb_print_line)(int row_index, int indicated, int length, void *data_source);
+
+enum action_return_values {
+   ARV_CONTINUE = 0,
+   ARV_REPLOT_DATA,
+   ARV_EXIT
+};
+typedef enum action_return_values ARV;
+
+typedef ARV (*PACTION)(DPARMS*);
+
+struct pager_keymap {
+   int action_index;         ///< index into array of function pointers
+   const char *capname;      ///< termcap name for key
+   const char *value;        ///< string emitted when the key is pressed
+};
+
+
 
 struct display_params {
    int reserve_top;         ///< lines at top left alone
@@ -12,18 +50,55 @@ struct display_params {
    int reserve_left;        ///< chars to left left alone
 
    int index_row_top;       ///< index number of top row of source
-   int index_row_selected;  ///< index of row that is currently 'selected'
+   int index_row_focus;     ///< index of row that currently has focus'
    int row_count;           ///< number of rows in the source
 
    void *data_source;       ///< passed to print function for printing lines
    pwb_print_line printer;  ///< function pointer to be called for each output line
+   const PACTION *actions;  ///< function table of actions to be performed on matching keystroke
+   const PKMAP *keymap;     ///< 'table' associating keystrokes to indexes to a function
+                            ///  table of actions
 
    int line_top;            ///< screen line to begin printing
    int line_count;          ///< number of screen lines in region
    int chars_left;          ///< left margin
-   int chars_count;         ///< number of characters to print per linecsscss
+   int chars_count;         ///< number of characters to print per line
 };
 
+extern PKMAP sel_keymap[];
+extern PACTION const sel_actionmap[];
+
+void update_keymap_values(PKMAP *kmap, int el_size);
 void update_display_params(DPARMS *params);
+void print_indexed_row(const DPARMS *parms, int row_index, bool has_focus);
+PACTION page_get_action(const DPARMS *parms, const char *keystroke);
+
+void scroll_line_up(const DPARMS *parms);
+void scroll_line_down(const DPARMS *parms);
+
 void print_page(DPARMS *params);
+
+void start_pager(DPARMS *params);
+
+
+
+bool row_index_is_visible(const DPARMS *parms, int row_index);
+int line_index_from_row_index(const DPARMS *parms, int row_index);
+
+ARV pager_quit(DPARMS *parms);
+ARV pager_activate(DPARMS *parms);
+
+ARV pager_focus_down_one(DPARMS *parms);
+ARV pager_focus_up_one(DPARMS *parms);
+ARV pager_focus_down_page(DPARMS *parms);
+ARV pager_focus_up_page(DPARMS *parms);
+ARV pager_focus_end(DPARMS *parms);
+ARV pager_focus_home(DPARMS *parms);
+ARV pager_scroll_down_one(DPARMS *parms);
+ARV pager_scroll_up_one(DPARMS *parms);
+ARV pager_scroll_down_page(DPARMS *parms);
+ARV pager_scroll_up_page(DPARMS *parms);
+ARV pager_scroll_end(DPARMS *parms);
+ARV pager_scroll_home(DPARMS *parms);
+
 #endif
