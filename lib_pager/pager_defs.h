278c0c97976ef96cf2c92c4f1ad34004664fe7ea
Chuck Jungmann <chuck@cpjj.net>
2023-12-16 07:05:35 -0600

    removed function-pointer struct and macros targeting them

--- lib_pager/pager_defs.h
+++ lib_pager/pager_defs.h
@@ -17,28 +17,6 @@ typedef enum action_return_values {
  */
 typedef ARV (*PACTION)(DPARMS*);
 
-
-typedef void(*pwb_getter)(int *row, int *col);
-typedef void(*pwb_two_ints)(int ione, int itwo);
-typedef void(*pwb_screener)(void);
-typedef struct pwb_screen_funcs {
-   pwb_two_ints    mover;           ///< function to move the cursor
-   pwb_getter      size_getter;     ///< function to get screen size
-   pwb_two_ints    limiter;         ///< function to set scrolling limits
-   pwb_screener    unlimiter;       ///< clear scrolling limits
-   pwb_screener    hide_c;          ///< function to hide cursor
-   pwb_screener    restore_c;       ///< function to restore cursor
-   pwb_screener    forward_one;
-   pwb_screener    reverse_one;
-} SCR_FUNCS;
-
-#define PARAM_GET_SCREEN_SIZE(p, rows, cols) (*(p)->funcs->size_getter)((rows),(cols))
-#define PARAM_SET_SCROLL_LIMIT(p, rows, cols) (*(p)->funcs->limiter)((rows),(cols))
-#define PARAM_CLEAR_SCROLL_LIMIT(p) (*(p)->funcs->unlimiter)()
-#define PARAM_MOVE(p,row,col) (*(p)->funcs->mover)((row),(col))
-#define PARAM_SCROLL_FORWARD(p) (*(p)->funcs->forward_one)()
-#define PARAM_SCROLL_REVERSE(p) (*(p)->funcs->reverse_one)()
-
 struct display_params {
    // Default top and focus rows is 0 (first record of data source):
    int index_row_top;       ///< index number of top row of source
@@ -48,7 +26,6 @@ struct display_params {
    void *data_source;       ///< passed to print function for printing lines
    int row_count;           ///< number of rows in the source
    pwb_print_line printer;  ///< function pointer to be called for each output line
-   SCR_FUNCS *funcs;        ///< struct with several screen-manipulation function pointers
 
 
    // Default values of 0, use function set_screen_margins() to change
