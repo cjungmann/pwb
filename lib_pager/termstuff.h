45886e61b0d82a02f6ad2b0e6ac739d5ab8f944d
Chuck Jungmann <chuck@cpjj.net>
2023-12-18 06:30:23 -0600

    Reorder prototypes to match appearance of functions in source code.

--- lib_pager/termstuff.h
+++ lib_pager/termstuff.h
@@ -12,11 +12,13 @@ void ti_get_cursor_position(int *row, int *col);
 void ti_get_screen_size(int *rows, int *cols);
 
 void ti_set_scroll_limit(int top, int count);
-void ti_scroll_forward(void);
-void ti_scroll_reverse(void);
 
 void ti_start_standout(void);
 void ti_end_standout(void);
+
+void ti_scroll_forward(void);
+void ti_scroll_reverse(void);
+
 
 
 #endif
