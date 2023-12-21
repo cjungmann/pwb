b35faa8fd00667f7c3edd66d725030a31ff2e4c7
Chuck Jungmann <chuck@cpjj.net>
2023-12-20 18:58:02 -0600

    prototypes for new visible functions

--- lib_pager/termstuff.h
+++ lib_pager/termstuff.h
@@ -8,13 +8,16 @@ int ti_printf(const char *fmt, ...);
 void ti_start_term(void);
 void ti_cleanup_term(void);
 
-void ti_reset_screen(void);
+void ti_reset_screen(const char *cmd);
 void ti_set_cursor_position(int row, int col);
 void ti_get_cursor_position(int *row, int *col);
 
 void ti_get_screen_size(int *rows, int *cols);
 
 void ti_set_scroll_limit(int top, int count);
+
+void ti_hide_cursor(void);
+void ti_show_cursor(void);
 
 void ti_start_standout(void);
 void ti_end_standout(void);
