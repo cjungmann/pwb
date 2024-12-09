3da0a26d588e3629fa3458d6dc8e515d6bb84d29
Chuck Jungmann <chuck@cpjj.net>
2024-12-09 10:31:28 -0600

    Minor refactor to better handle replacing the basic keymap.
    Using pointers for the basic keymap to allow the singleton
    default keymap to coexist with potential alternates.  Then
    changed the aux keymap to use a pointer for consistency and
    to qualify use outside of the while loop.

--- pwb_action_start.c
+++ pwb_action_start.c
@@ -15,16 +15,20 @@ PWB_RESULT pwb_action_start(PWBH *handle, ACLONE *args)
 
    unsigned int top_row = -1;
    unsigned int focus_row = -1;
+
+   // Singleton, reuse when possible:
+   KCLASS default_keymap = { 0 };
+
    KCLASS base_keymap = { 0 };
    KCLASS aux_keymap = { 0 };
    const char *help_flag = NULL;
 
    // Initialize singleton (static) base_keymap only once:
-   if (base_keymap.data == NULL)
+   if (default_keymap.data == NULL)
    {
       // Don't disturb the 'result' default value of PWB_FAILURE
       // with a PWB_SUCCESS return of initialize_kclass:
-      if (PWB_SUCCESS != initialize_kclass(&base_keymap, (*get_default_kdata)()))
+      if (PWB_SUCCESS != initialize_kclass(&default_keymap, (*get_default_kdata)()))
       {
          (*error_sink)("Unexpected failure during global kclass initialization.");
          return PWB_FAILURE;
@@ -70,6 +74,18 @@ PWB_RESULT pwb_action_start(PWBH *handle, ACLONE *args)
          pwbh_calc_borders(handle);
 
          ARV arv = ARV_REPLOT_DATA;
+
+         // Sort out which keymaps to use here, outside of the while look
+         KCLASS *basekm = NULL;
+         KCLASS *auxkm = NULL;
+         if ( base_keymap.data && *base_keymap.entry_count>0 )
+            basekm = &base_keymap;
+         else
+            basekm = &default_keymap;
+
+         if ( aux_keymap.data && *aux_keymap.entry_count > 0 )
+            auxkm = &aux_keymap;
+
          while (arv != ARV_EXIT)
          {
             if (arv == ARV_REPLOT_DATA)
@@ -77,7 +93,7 @@ PWB_RESULT pwb_action_start(PWBH *handle, ACLONE *args)
 
             const char *keys = get_keystroke(NULL, 0);
             if (keys)
-               arv = pwb_run_keystroke(handle, keys, &base_keymap, &aux_keymap);
+               arv = pwb_run_keystroke(handle, keys, basekm, auxkm);
          }
       }
    }
